4197b555fe6ea5e5d4bef0aeda14e710d2aea0c3
Cezar Augusto <boss@cezaraugusto.net>
2019-03-15 15:15:06 -0300

    separate other data API from storage

--- components/brave_new_tab_ui/api/data.ts
+++ components/brave_new_tab_ui/api/data.ts
@@ -5,6 +5,10 @@
 import { bindActionCreators } from 'redux'
 import * as newTabActions from '../actions/new_tab_actions'
 import store from '../store'
+import { defaultState } from '../storage'
+import * as backgroundAPI from './background'
+import { debounce } from '../../common/debounce'
+const keyName = 'new-tab-data'
 
 /**
  * Get actions from the C++ back-end down to front-end components
@@ -26,3 +30,54 @@ export const fetchTopSites = () => {
     return getActions().topSitesDataUpdated(topSites || [])
   })
 }
+
+/**
+ * Get initial data from the back-end
+ * @param state - The application state
+ */
+export const getLoadTimeData = (state: NewTab.State) => {
+  state = { ...state }
+  state.stats = defaultState.stats
+  ;['adsBlockedStat', 'trackersBlockedStat', 'javascriptBlockedStat',
+    'httpsUpgradesStat', 'fingerprintingBlockedStat'].forEach((stat) => {
+      state.stats[stat] = parseInt(chrome.getVariableValue(stat), 10) || 0
+    })
+  state.useAlternativePrivateSearchEngine = chrome.getVariableValue('useAlternativePrivateSearchEngine') === 'true'
+  return state
+}
+
+/**
+ * Get initial background data
+ * @param state - The application state
+ */
+const cleanData = (state: NewTab.State): NewTab.State => {
+  state = { ...state }
+  state.backgroundImage = backgroundAPI.randomBackgroundImage()
+  state = getLoadTimeData(state)
+  return state
+}
+
+/**
+ * Get and load all items from localStorage
+ */
+export const load = (): NewTab.State => {
+  const data = window.localStorage.getItem(keyName)
+  let state = defaultState
+  if (data) {
+    try {
+      state = JSON.parse(data)
+    } catch (e) {
+      console.error('Could not parse local storage data: ', e)
+    }
+  }
+  return cleanData(state)
+}
+
+/**
+ * Set all items in localStorage
+ */
+export const debouncedSave = debounce<NewTab.State>((data: NewTab.State) => {
+  if (data) {
+    window.localStorage.setItem(keyName, JSON.stringify(cleanData(data)))
+  }
+}, 50)
--- components/brave_new_tab_ui/storage.ts
+++ components/brave_new_tab_ui/storage.ts
@@ -2,14 +2,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import * as backgroundAPI from './api/background'
-
-// Utils
-import { debounce } from '../common/debounce'
-
-const keyName = 'new-tab-data'
-
-const defaultState: NewTab.State = {
+/**
+ * Initial state when profile is empty
+ * This will be later populated with back-end information
+ * such as default search engine and stats
+ */
+export const defaultState: NewTab.State = {
   topSites: [],
   ignoredTopSites: [],
   pinnedTopSites: [],
@@ -28,40 +26,3 @@ const defaultState: NewTab.State = {
     fingerprintingBlockedStat: 0
   }
 }
-
-export const getLoadTimeData = (state: NewTab.State) => {
-  state = { ...state }
-  state.stats = defaultState.stats
-  ;['adsBlockedStat', 'trackersBlockedStat', 'javascriptBlockedStat',
-    'httpsUpgradesStat', 'fingerprintingBlockedStat'].forEach((stat) => {
-      state.stats[stat] = parseInt(chrome.getVariableValue(stat), 10) || 0
-    })
-  state.useAlternativePrivateSearchEngine = chrome.getVariableValue('useAlternativePrivateSearchEngine') === 'true'
-  return state
-}
-
-const cleanData = (state: NewTab.State): NewTab.State => {
-  state = { ...state }
-  state.backgroundImage = backgroundAPI.randomBackgroundImage()
-  state = getLoadTimeData(state)
-  return state
-}
-
-export const load = (): NewTab.State => {
-  const data = window.localStorage.getItem(keyName)
-  let state = defaultState
-  if (data) {
-    try {
-      state = JSON.parse(data)
-    } catch (e) {
-      console.error('Could not parse local storage data: ', e)
-    }
-  }
-  return cleanData(state)
-}
-
-export const debouncedSave = debounce<NewTab.State>((data: NewTab.State) => {
-  if (data) {
-    window.localStorage.setItem(keyName, JSON.stringify(cleanData(data)))
-  }
-}, 50)
